c9707950ed19d4f5fd7c734a88860df04f1e1f50
HarderThenICan <explorer676@gmail.com>
2021-08-31 18:50:25 +0300

    Домашнее задание №8

---
+++ src/HW8/Listener.java
@@ -0,0 +1,12 @@
+package HW8;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Listener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+
+    }
+}
